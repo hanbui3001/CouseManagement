25de63c0d2b5188de652bd5c82f052fecae27c96
Han Bui <bronzegoemo@gmail.com>
2025-07-18 21:19:37 +0700

    init project

---
+++ src/main/java/courseProject/fullSV/FullSvApplication.java
@@ -0,0 +1,13 @@
+package courseProject.fullSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/courseProject/fullSV/FullSvApplicationTests.java
@@ -0,0 +1,13 @@
+package courseProject.fullSV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
